79aae4f11c1380b164e3aaab8250065655675ad9
Dawid Chądzyński <dardaw@gmail.com>
2024-02-04 09:35:30 +0100

    aktualizacja generowania pliku

--- generuj_plik.php
+++ generuj_plik.php
@@ -1,14 +1,14 @@
 <?php
 
-$plik = file('http://www.mbnet.com.pl/dl.txt');
+$plik = file('http://www.mbnet.com.pl/el.txt');
 //6959. 09.11.2023 4,7,24,35,36,47
 $lista = [];
-$lista[0] = ['Game', 'Date', 'A', 'B', 'C', 'D', 'E', 'F'];
+$lista[0] = ['Game', 'Date', 'A', 'B', 'C', 'D', 'E'];
 foreach ($plik as $klucz => $linia) {
     list($nr, $data, $liczby) = explode(' ', $linia);
     $liczba = explode(',', $liczby);
     $data = str_replace('.', '/', $data);
-    $lista[$klucz + 1] = [str_replace('.', '', $nr), $data, $liczba[0], $liczba[1], $liczba[2], $liczba[3], $liczba[4], (int)$liczba[5]];
+    $lista[$klucz + 1] = [str_replace('.', '', $nr), $data, $liczba[0], $liczba[1], $liczba[2], $liczba[3], (int)$liczba[4]];
 }
 
 
